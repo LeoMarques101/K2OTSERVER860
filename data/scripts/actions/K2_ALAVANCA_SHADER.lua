aa178a6c4bbfa795bafa9b32f96a2c3a2c57c56c
Leonardo Marques <leomarques.tech"gmail.com>
2022-02-27 12:30:03 +0000

    pequenos ajustes

--- data/scripts/actions/K2_ALAVANCA_SHADER.lua
+++
@@ -1,9 +0,0 @@
-local shader = TalkAction("!shader")
-
-function shader.onSay(player, words, param, type)
-	player:say("Hello!", TALKTYPE_YELL)
-	player:setOutfitShader("line")
-	return false
-end
-
-shader:register()
