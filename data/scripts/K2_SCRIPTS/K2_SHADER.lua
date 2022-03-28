6f6a07660cd810055786086e82d5928060b314f0
Leonardo Marques <leomarques.tech"gmail.com>
2022-03-29 00:19:34 +0100

    dataremove

--- data/scripts/K2_SCRIPTS/K2_SHADER.lua
+++
@@ -1,9 +0,0 @@
-local talkaction = TalkAction("!hello")
-
-function talkaction.onSay(player, words, param, type)
-	player:sendExtendedOpcode(12, "teste")
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "enviado")
-	return false
-end
-
-talkaction:register()
